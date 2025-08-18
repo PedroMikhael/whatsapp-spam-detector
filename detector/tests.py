9e7ece1754293c0dc4af23cd69c575d9889b9b04
Pedro Mikhael <pedromikhaelsouza@gmail.com>
2025-08-18 15:37:19 -0300

    Finalizada a rota para verificar spam

--- detector/tests.py
+++ detector/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from detector.services import verificar_texto_spam
 
-# Create your tests here.
+class SpamTestCase(TestCase):
+    def test_texto_spam(self):
+        resultado = verificar_texto_spam("Ganhe dinheiro agora!")
+        self.assertTrue(resultado["spam"])
+
+    def test_texto_legitimo(self):
+        resultado = verificar_texto_spam("Oi, tudo bem?")
+        self.assertFalse(resultado["spam"])
