4220aab449d51e246a94eeafa4589e5ee0031eb9
Pedro Mikhael <pedromikhaelsouza@gmail.com>
2025-08-22 19:31:28 -0300

    resolvido conflito

--- spamapi/settings.py
+++ spamapi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(bcp0_$=z9nylls(cp8p%!it+*(d2ap3p(9t40^%s@_gg+1t#f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["18.222.191.70", "localhost",]
 
 
 # Application definition
