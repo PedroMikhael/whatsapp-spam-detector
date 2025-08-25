de0c73a1d0ca9cb470436d8589ca2d269be6cc1f
Pedro Mikhael <pedromikhaelsouza@gmail.com>
2025-08-25 14:28:42 -0300

    adicionando novas views

--- spamapi/urls.py
+++ spamapi/urls.py
@@ -4,6 +4,7 @@ from detector.views import verificar_spam
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.views.generic import RedirectView
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -12,10 +13,11 @@ schema_view = get_schema_view(
       description="API para detectar spam em mensagens",
    ),
    public=True,
-   permission_classes=(permissions.AllowAny,),
+   permission_classes=(permissions.AllowAny,),  # garante acesso público
 )
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/swagger/', permanent=True)),  # redireciona / para swagger
     path('admin/', admin.site.urls),
     path('api/spam/', verificar_spam, name="verificar_spam"),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
