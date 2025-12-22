e36a388ccb9a383a6e43bc84e549c99ff84de11d
Pedro Mikhael <pedromikhaelsouza@gmail.com>
2025-12-22 17:28:05 -0300

    Utilizando Ollama para testes locais

--- detector/services.py
+++ detector/services.py
@@ -120,7 +120,7 @@ def analisar_com_gemini(texto: str) -> dict:
     """
     
     try:
-        model = genai.GenerativeModel('gemini-1.5-flash')
+        model = genai.GenerativeModel('gemini-2.5-flash')
         response = model.generate_content(prompt)
         cleaned_response = response.text.strip().replace("`", "").replace("json", "")
         match = re.search(r'\{.*\}', cleaned_response, re.DOTALL)
